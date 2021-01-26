2478473c5b975e691ef7d3f7c590a844c51c2cb1
suzukishouta <77910581+suzukishouta@users.noreply.github.com>
2021-01-26 11:10:00 +0900

    Extract CSS and HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -11,7 +13,7 @@ module.exports = {
 				test: /\.css/,
 				use:[
           {
-            loader: 'style-loader',
+          loader: MiniCssExtractPlugin.loader,
           },
 					{
 						loader: 'css-loader',
@@ -19,5 +21,11 @@ module.exports = {
 				],
 			},
 		],
-	},
+  },
+  plugins:[
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 }
